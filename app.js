9ba1cb7656298075bc8575be69eeec30e24f5f5a
shourov <shourovys@gmail.com>
2020-06-27 23:48:03 +0600

    all basic functionality added

---
+++ app.js
@@ -0,0 +1,94 @@
+// call all task form localStorage for show 1st
+showNotes()
+
+// if user add a note, add it localStorage
+// add event lisente in #add-btn for gating text form #add-text
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", function () {
+    let addText = document.getElementById('add-text')
+    // gating value form localStorage
+    let notes = localStorage.getItem("notes")
+    let noteObj = []
+    if (notes == null) {
+        noteObj = []
+    }
+    else {
+        noteObj = JSON.parse(notes)
+    }
+    noteObj.push(addText.value)
+    localStorage.setItem("notes", JSON.stringify(noteObj))
+    addText.value = ""
+    showNotes()
+})
+
+// function that get data form localStorage and show note carts
+function showNotes() {
+    let notes = localStorage.getItem("notes")
+    let noteObj = []
+    if (notes == null) {
+        noteObj = []
+    }
+    else {
+        noteObj = JSON.parse(notes)
+    }
+    html = ''
+    noteObj.forEach((note, index) => {
+        html += ` 
+        <div  class="card cart m-2" style="width: 22.48rem;">
+            <div class="card-body cart">
+                <h5 class="card-title">Note:${index + 1}</h5>
+                <p class="card-text">${note}</p>
+                <a id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete</a>
+            </div>
+        </div>`
+
+    });
+    //  getting the cart container and update it's HTML
+    const noteContainer = document.getElementById("note-container")
+    if (noteObj.length != 0) {
+        noteContainer.innerHTML = html
+    }
+    else {
+        noteContainer.innerHTML = `&nbsp &nbsp No Task added. Please try Add Note button`
+    }
+}
+
+// delete test cart by clicking delete button 
+
+function deleteNote(id) {
+    let notes = localStorage.getItem("notes")
+    let noteObj = [];
+    if (notes == null) {
+        noteObj = []
+    }
+    else {
+        noteObj = JSON.parse(notes)
+    }
+    noteObj.splice(id, 1)
+
+    localStorage.setItem("notes", JSON.stringify(noteObj))
+    showNotes()
+}
+
+// search cart by search 
+
+let searchText = document.getElementById("searchText");
+searchText.addEventListener("input", function () {
+    let inputText = searchText.value.toLowerCase()
+    let carts = document.getElementsByClassName("cart")
+
+    Array.from(carts).forEach(cart => {
+        let cartText = cart.querySelectorAll("P")[0]
+        cartText = cartText.innerText;
+        // console.log(cartText);
+        if (cartText.includes(inputText)) {
+            cart.style.display = "block"
+        }
+        else {
+            cart.style.display = "none"
+        }
+    });
+
+})
+
+
